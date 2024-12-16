4ed8895568d4b5523b04a4a887c978fb91ab5d81
nkucss <164296415+nkucss@users.noreply.github.com>
2024-12-16 15:46:50 +0800

    Add files via upload

---
+++ lab5/obj/kern/mm/cow.d
@@ -0,0 +1,6 @@
+obj/kern/mm/cow.o obj/kern/mm/cow.d: kern/mm/cow.c kern/mm/cow.h \
+ libs/defs.h kern/mm/mmu.h kern/mm/vmm.h libs/list.h kern/mm/memlayout.h \
+ libs/atomic.h kern/sync/sync.h kern/driver/intr.h kern/schedule/sched.h \
+ kern/process/proc.h kern/trap/trap.h libs/riscv.h kern/debug/assert.h \
+ kern/mm/kmalloc.h libs/string.h kern/mm/pmm.h libs/error.h libs/elf.h \
+ libs/stdio.h libs/stdarg.h libs/stdlib.h libs/unistd.h
